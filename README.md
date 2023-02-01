377efc704743373b938bea5924bb8255b21182d0
m2gi-behraf <43551715+m2gi-behraf@users.noreply.github.com>
2023-02-01 15:33:00 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# m2gi-devMobile-tuto-prog-reactive
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/demo-prog-reactive-ac3tf9)
---
+++ src/app/github-api.service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, combineLatest,  ConnectableObservable, Observable, of, Subject } from 'rxjs';
+import { fromPromise } from 'rxjs/internal-compatibility';
+import { map, multicast,  refCount,    scan, startWith,  switchMap, switchMapTo, tap } from 'rxjs/operators';
+
+/*
+ * Voir https://docs.github.com/en/free-pro-team@latest/rest/reference/users
+ */
+@Injectable()
+export class GithubApiService {
+  private refreshSubj = new Subject<void>    ();
+  private deletedSubj = new Subject<DataUser>();
+
+  public usersObs: Observable<DataUser[]>;
+
+  constructor() {
+    const Lall = this.refreshSubj.pipe(
+      switchMap( () => fromPromise(this.getUsers()) ),
+    );
+
+    const Ldeleted: Observable<DataUser[]> = this.refreshSubj.pipe(
+      switchMap( () => this.deletedSubj.pipe(
+        scan( (L, u) => [...L, u], [] ),
+        startWith([])
+      ) )
+    );
+
+    this.usersObs = of([]); // XXX à modifier...
+    
+    this.refresh();
+  }
+
+  async getUsers(): Promise<DataUser[]> {
+    const R = await fetch(`https://api.github.com/users?since=${1000*Math.random()}`);
+    if (R.status >= 200 && R.status < 300) {
+      const L: DataUser[] = await R.json(); // any -> DataUser[]
+      return L;
+    } else {
+      return [{login: 'Bob'}, {login: "Jo"}, {login: 'Avrel'}, {login: "Tao"}, {login: 'Kenzi'}, {login: "Jacques"}].sort(
+        (a, b) => Math.round( 2*Math.random() - 1 )
+      );
+    }
+  }
+  
+  refresh(): void {
+    this.refreshSubj.next();
+  }
+
+  deleteUser(u: DataUser) {
+    this.deletedSubj.next(u);
+  }
+}
+
+export interface DataUser {
+  login: string;
+}
+
